a5fb88d4d82bc2f0a80c9745ad4d320e699ca777
jdjes <Jdjessee@gmail.com>
2026-08-24 14:10:02 -0700

    Bump SW v10 to force stale-worker refresh

--- pwa/sw.js
+++ pwa/sw.js
@@ -22,7 +22,7 @@
  *      (so subsequent offline loads get fresh content)
  */
 
-const CACHE_NAME = 'coffee-ratio-v9';
+const CACHE_NAME = 'coffee-ratio-v10';
 const ASSETS_TO_CACHE = [
   'index.html',
   'styles.css',
