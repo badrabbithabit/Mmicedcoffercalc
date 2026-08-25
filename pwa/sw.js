0e91caa86811da156ad054445d0adc27522d5db8
jdjes <Jdjessee@gmail.com>
2026-08-25 03:56:02 -0700

    Add lighter-black panels around title+recipe and quote

--- pwa/sw.js
+++ pwa/sw.js
@@ -22,7 +22,7 @@
  *      (so subsequent offline loads get fresh content)
  */
 
-const CACHE_NAME = 'coffee-ratio-v10';
+const CACHE_NAME = 'coffee-ratio-v11';
 const ASSETS_TO_CACHE = [
   'index.html',
   'styles.css',
